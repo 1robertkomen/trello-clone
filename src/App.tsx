db5614718be2692ce28fe4523bfd480d8a9a3842
aitumik <aitumik@protonmail.com>
2022-02-22 02:10:08 +0300

    feat(app): add rows for displaying tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { FC } from "react"
 import { Column } from "./Column"
-import { Card } from "./card"
+import { Card } from "./Card"
+import { AddNewItem } from "./AddNewItem"
 import {AppContainer} from "./styles";
 
-export const App : FC = ({children}) => {
+export const App = () => {
   return (
     <AppContainer>
-      Introduction to trello
+      <Column text="To Do">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="In progress">
+        <Card text="Create backend in go" />
+      </Column>
+      <Column text="Done">
+        <Card text="Watch the Matrix ressurection" />
+      </Column>
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log}/>
     </AppContainer>
   )
 }
-
