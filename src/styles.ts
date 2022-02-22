cc33920718369cd6696265eab5a87635c59b2370
Robert <robert@Jk-MacBook-Pro.local>
2022-02-22 20:59:08 +0300

    fix: add styles.ts and app layout.

---
+++ src/styles.ts
@@ -0,0 +1,25 @@
+
+import styled from 'styled-components'
+export const AppContainer = styled.div
+`  
+bacgroundd-color:red;
+width: 300px;
+min-height: 40px;
+bordr-radius:3px;
+padding: 10px 10px
+flex-gro:0
+
+`
+export const ColumnTitle = styled.div`
+    padding: 6px 16px 12px;
+    font-weight: bold;
+    `
+
+export const CardContainer = styled.div`
+    background-color: #fff;
+    cursor: pointer;
+    margin-botttom: 0.5rem;
+    max-width: 300px;
+    border-radius: 3px;
+    box-shadow: #091e4240 0px 1px 0px 0px;
+    `
